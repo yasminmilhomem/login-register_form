9ed0e4158e2f4b945ae097991f08646ee2083702
yasminmilhomem <yasminmilhomem1517@gmail.com>
2022-12-28 21:20:44 -0300

    eighth commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let signUpBtn = document.getElementById("signUpBtn");
+let signInBtn = document.getElementById("signInBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+signInBtn.onclick = function(){
+    nameField.style.maxHeight = "0";
+}
