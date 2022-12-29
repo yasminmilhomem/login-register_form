cb5e4e084848fc793c1282c13fbc6a2e086c5a73
yasminmilhomem <yasminmilhomem1517@gmail.com>
2022-12-29 16:20:51 -0300

    ninth commit

--- js/script.js
+++ js/script.js
@@ -5,4 +5,14 @@ let title = document.getElementById("title");
 
 signInBtn.onclick = function(){
     nameField.style.maxHeight = "0";
+    title.innerHTML = "Sign In";
+    signUpBtn.classList.add("disabled");
+    signInBtn.classList.remove("disabled");
+}
+
+signUpBtn.onclick = function(){
+    nameField.style.maxHeight = "3.75rem";
+    title.innerHTML = "Sign Up";
+    signUpBtn.classList.remove("disabled");
+    signInBtn.classList.add("disabled");
 }
